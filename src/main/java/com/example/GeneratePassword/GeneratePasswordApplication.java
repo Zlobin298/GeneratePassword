c7b122fee33dc3a30fca6b4f50cd572202035db5
opank <opankov052@gmail.com>
2026-01-31 01:53:43 +0500

    Initial commit

---
+++ src/main/java/com/example/GeneratePassword/GeneratePasswordApplication.java
@@ -0,0 +1,13 @@
+package com.example.GeneratePassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratePasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratePasswordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GeneratePassword/GeneratePasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GeneratePassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratePasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
